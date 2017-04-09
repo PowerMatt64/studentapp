cf21cce26d049c785781dddcd820879b901de292
David Evans <David Evans>
2017-04-09 12:05:51 -0700

    menu

--- gui/src/Main.js
+++ gui/src/Main.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Students from './Students';
 import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import ActionExit from 'material-ui/svg-icons/action/exit-to-app';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import Menu from 'material-ui/Menu';
+import FontIcon from 'material-ui/FontIcon';
+import ReactDOM from 'react-dom';
+
 
 
 //const host = "";
@@ -11,6 +19,7 @@ export default class Main extends React.Component {
 	constructor(props) {
 		super(props);
         this.state = {
+			open: false
         }
     }
 
@@ -18,13 +27,40 @@ export default class Main extends React.Component {
     componentDidMount() {
     }
 
+    // handle menu
+    handleHamburgerMenuClick() {
+		this.setState({open: !this.state.open})
+	}
+
+	routeHandler(event, menu, obj) {
+		ReactDOM.findDOMNode(this.refs.students).style.display='none';
+		if (menu==='studentsMenu') ReactDOM.findDOMNode(this.refs.students).style.display='block';
+		this.setState({open: false});
+	}
+
     render() {
+		var _this = this;
+        const contentStyle = { display:'none', transition: 'margin-left 450ms cubic-bezier(0.23, 1, 0.32, 1)' };
+        if (this.state.open) {
+            contentStyle.marginLeft = 256;
+        }
 
         return (
             <div>
 
-                <AppBar title="Simple Student App" />
-				<Students />
+                <AppBar title="Simple Student App" onLeftIconButtonTouchTap={this.handleHamburgerMenuClick.bind(this)}
+                			iconElementRight={<IconButton href="/logout" tooltip="Logout"><ActionExit /></IconButton>}/>
+
+
+				<Drawer open={this.state.open} docked={false}>
+					<AppBar title="Menu" showMenuIconButton={false}/>
+					<Menu onChange={this.routeHandler.bind(this)}>
+						<MenuItem value="studentsMenu" leftIcon={<FontIcon className="material-icons" >supervisor_account</FontIcon>}>Students</MenuItem>
+                    </Menu>
+
+                </Drawer>
+
+				<div ref="students" style={contentStyle}><Students/></div>
 
             </div>
         )
